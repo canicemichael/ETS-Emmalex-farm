027d81540a4b8915a789f081638e60eac1b3aa99
canicemichael <canicemichael@gmail.com>
2022-09-02 14:38:33 +0100

    Add landing-page blog post section

--- frontend/scripts/landing_page/index.js
+++ frontend/scripts/landing_page/index.js
@@ -1,33 +1,3 @@
-// var multipleCardCarousel = document.querySelector("#carouselExampleControls");
-// if (window.matchMedia("(min-width: 768px)").matches) {
-//   var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-//     interval: false,
-//   });
-//   var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-//   var cardWidth = $(".carousel-item").width();
-//   var scrollPosition = 0;
-//   $("#carouselExampleControls .carousel-control-next").on("click", function () {
-//     if (scrollPosition < carouselWidth - cardWidth * 4) {
-//       scrollPosition += cardWidth;
-//       $("#carouselExampleControls .carousel-inner").animate(
-//         { scrollLeft: scrollPosition },
-//         600
-//       );
-//     }
-//   });
-//   $("#carouselExampleControls .carousel-control-prev").on("click", function () {
-//     if (scrollPosition > 0) {
-//       scrollPosition -= cardWidth;
-//       $("#carouselExampleControls .carousel-inner").animate(
-//         { scrollLeft: scrollPosition },
-//         600
-//       );
-//     }
-//   });
-// } else {
-//   $(multipleCardCarousel).addClass("slide");
-// }
-
 // section home
 
 let vid = document.getElementById("myVideo");
@@ -37,3 +7,39 @@ function setPlaySpeed() {
 }
 
 setPlaySpeed();
+
+// section our success gallery
+let allHomeGallery = document.getElementsByClassName("home_gallery_pht_1");
+let allWorkBtn = document.getElementById("all-work-btn");
+let platedGrassBtn = document.getElementById("planted-grass-btn");
+let feedBtn = document.getElementById("feed-btn");
+let medicineBtn = document.getElementById("medicine-btn");
+let stableBtn = document.getElementById("stable-btn");
+let abbatoirBtn = document.getElementById("abbatoir-btn");
+
+let allWork = document.getElementsById("all-work");
+let abbatoir = document.getElementsById("abbatoir");
+// let farmCrop = document.getElementsById("farm-crop");
+let feed = document.getElementsById("feed");
+let medicine = document.getElementsById("medicine");
+let stable = document.getElementsById("stable");
+
+let allBtns = document.querySelectorAll("button");
+
+// For each button, register an event listener
+// allBtns.forEach(function (elem) {
+//   elem.addEventListener("click", function (e) {
+//     // On click, remove the MyClass on ALL buttons
+//     allBtns.forEach(function (el) {
+//       el.classList.remove("MyClass");
+//     });
+
+//     // Add the class on clicked one
+//     e.target.classList.add("MyClass");
+
+//     // Now pass the data-href to your iframe
+//     let theHREFtoOpen = e.target.getAttribute("data-href");
+//     console.log(theHREFtoOpen);
+//     //document.querySelector("#your-iframe").src = theHREFtoOpen
+//   });
+// });
