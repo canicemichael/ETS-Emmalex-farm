364a0f489eb1cfe8ca49174f78b87a42e84b5021
canicemichael <canicemichael@gmail.com>
2022-09-13 10:30:51 +0100

    update gallery page

---
+++ frontend/scripts/gallery.js
@@ -0,0 +1,226 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+var img = document.querySelector(".donkey-stable-img");
+var img2 = document.querySelector(".donkey-stable-img2");
+var img3 = document.querySelector(".donkey-stable-img3");
+var img4 = document.querySelector(".donkey-stable-img4");
+var img5 = document.querySelector(".donkey-stable-img5");
+var img6 = document.querySelector(".donkey-stable-img6");
+var img7 = document.querySelector(".donkey-stable-img7");
+var img8 = document.querySelector(".donkey-stable-img8");
+var img9 = document.querySelector(".donkey-stable-img9");
+var img10 = document.querySelector(".donkey-stable-img10");
+var img11 = document.querySelector(".donkey-stable-img11");
+var img12 = document.querySelector(".donkey-stable-img12");
+var img13 = document.querySelector(".donkey-stable-img13");
+var img14 = document.querySelector(".donkey-stable-img14");
+var img15 = document.querySelector(".donkey-stable-img15");
+var img16 = document.querySelector(".donkey-stable-img16");
+var img17 = document.querySelector(".donkey-stable-img17");
+var img18 = document.querySelector(".donkey-stable-img18");
+var img19 = document.querySelector(".donkey-stable-img19");
+var img20 = document.querySelector(".donkey-stable-img20");
+var img21 = document.querySelector(".donkey-stable-img21");
+var img22 = document.querySelector(".donkey-stable-img22");
+var img23 = document.querySelector(".donkey-stable-img23");
+var img24 = document.querySelector(".donkey-stable-img24");
+var img25 = document.querySelector(".donkey-stable-img25");
+var img26 = document.querySelector(".donkey-stable-img26");
+var img27 = document.querySelector(".donkey-stable-img27");
+var img28 = document.querySelector(".donkey-stable-img28");
+var img29 = document.querySelector(".donkey-stable-img29");
+var img30 = document.querySelector(".donkey-stable-img30");
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img2.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img3.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img4.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img5.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img6.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img7.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img8.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img9.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img10.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img11.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img12.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img13.onclick = function () {
+  modal.style.display = "block";
+
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img14.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img15.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img16.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img17.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img18.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img19.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img20.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img21.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img22.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img23.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img24.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img25.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img26.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img27.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img28.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img29.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+img30.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
