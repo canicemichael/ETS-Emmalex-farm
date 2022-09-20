9a0d370affff45d486721c1eeef951dafbb05c15
unknown <bsaheed79@gmail.com>
2022-09-20 03:03:09 -0700

    completed work

---
+++ frontend/scripts/booking.js
@@ -0,0 +1,47 @@
+let LiveDonkeyDetails = document.querySelector(".LiveDonkey");
+let DonkeyMeatDetails = document.querySelector(".DonkeyMeat");
+let DonkeyMilkDetails = document.querySelector(".DonkeyMilk");
+
+let LiveDonkey = document.querySelector("#live_donkey");
+let DonkeyMeat = document.querySelector("#donkey_meat");
+let DonkeyMilk = document.querySelector("#donkey_milk");
+let Adoption = document.querySelector(".Adoption");
+let Excursions = document.querySelector(".Excursions");
+let excursionchecked = document.querySelector("#excursion");
+let adoptchecked = document.querySelector("#adopt");
+
+excursionchecked.addEventListener("click", () => {
+  if (excursionchecked.checked === true) {
+    Excursions.style.display = "flex";
+  } else {
+    Excursions.style.display = "none";
+  }
+});
+adoptchecked.addEventListener("click", () => {
+  if (adoptchecked.checked === true) {
+    Adoption.style.display = "block";
+  } else {
+    Adoption.style.display = "none";
+  }
+});
+LiveDonkey.addEventListener("click", () => {
+  if (LiveDonkey.checked === true) {
+    LiveDonkeyDetails.style.display = "block";
+  } else {
+    LiveDonkeyDetails.style.display = "none";
+  }
+});
+DonkeyMeat.addEventListener("click", () => {
+  if (DonkeyMeat.checked === true) {
+    DonkeyMeatDetails.style.display = "block";
+  } else {
+    DonkeyMeatDetails.style.display = "none";
+  }
+});
+DonkeyMilk.addEventListener("click", () => {
+  if (DonkeyMilk.checked === true) {
+    DonkeyMilkDetails.style.display = "block";
+  } else {
+    DonkeyMilkDetails.style.display = "none";
+  }
+});
